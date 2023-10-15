e8b12705cc064bb6d340d7fa99977b078db47450
CAR-hash <lejian663@126.com>
2023-10-15 21:55:53 +0800

    lora adapt

--- models/adam_finetune.py
+++ models/adam_finetune.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from utils.inc_net import IncrementalNet,SimpleCosineIncrementalNet,MultiBranchCosineIncrementalNet,SimpleVitNet
+from utils.inc_net import IncrementalNet, SimpleCosineIncrementalNet, MultiBranchCosineIncrementalNet, SimpleVitNet
 from models.base import BaseLearner
 from utils.toolkit import target2onehot, tensor2numpy
 from timm.scheduler import create_scheduler
@@ -17,81 +17,86 @@ import loralib as lora
 # fully finetune the model at first session, and then conduct simplecil.
 num_workers = 8
 
+
 class FocalLoss(nn.Module):
     def __init__(self):
         return
-    def forward(self,x, y):
+
+    def forward(self, x, y):
         return
+
 
 class Learner(BaseLearner):
     def __init__(self, args):
         super().__init__(args)
         if 'resnet' in args['convnet_type']:
             self._network = SimpleCosineIncrementalNet(args, True)
-            self. batch_size=128
-            self.init_lr=args["init_lr"] if args["init_lr"] is not None else  0.01
+            self.batch_size = 128
+            self.init_lr = args["init_lr"] if args["init_lr"] is not None else 0.01
         else:
             self._network = SimpleVitNet(args, True)
-            self. batch_size= args["batch_size"]
-            self. init_lr=args["init_lr"]
-        
-        self.weight_decay=args["weight_decay"] if args["weight_decay"] is not None else 0.0005
-        self.min_lr=args['min_lr'] if args['min_lr'] is not None else 1e-8
-        self.args=args
+            self.batch_size = args["batch_size"]
+            self.init_lr = args["init_lr"]
+
+        self.weight_decay = args["weight_decay"] if args["weight_decay"] is not None else 0.0005
+        self.min_lr = args['min_lr'] if args['min_lr'] is not None else 1e-8
+        self.args = args
 
         self._trsf = transforms.Compose(
             [
-                #transforms.ColorJitter(brightness=(0.9, 1.1), saturation=(0.9, 1.1))
+                # transforms.ColorJitter(brightness=(0.9, 1.1), saturation=(0.9, 1.1))
                 transforms.RandomHorizontalFlip(p=0.5)
             ]
         )
 
     def after_task(self):
         self._known_classes = self._total_classes
-    
-    def replace_fc(self,trainloader, model, args):
+
+    def replace_fc(self, trainloader, model, args):
         model = model.eval()
         embedding_list = []
         label_list = []
         with torch.no_grad():
             for i, batch in enumerate(trainloader):
-                (_,data,label)=batch
-                data=data.cuda()
-                label=label.cuda()
+                (_, data, label) = batch
+                data = data.cuda()
+                label = label.cuda()
                 embedding = model(data)['features']
                 embedding_list.append(embedding.cpu())
                 label_list.append(label.cpu())
         embedding_list = torch.cat(embedding_list, dim=0)
         label_list = torch.cat(label_list, dim=0)
 
-        class_list=np.unique(self.train_dataset.labels)
+        class_list = np.unique(self.train_dataset.labels)
         proto_list = []
         for class_index in class_list:
             # print('Replacing...',class_index)
-            data_index=(label_list==class_index).nonzero().squeeze(-1)
-            embedding=embedding_list[data_index]
-            proto=embedding.mean(0)
-            self._network.fc.weight.data[class_index]=proto
+            data_index = (label_list == class_index).nonzero().squeeze(-1)
+            embedding = embedding_list[data_index]
+            proto = embedding.mean(0)
+            self._network.fc.weight.data[class_index] = proto
         return model
 
-    
     def incremental_train(self, data_manager):
         self._cur_task += 1
         self._total_classes = self._known_classes + data_manager.get_task_size(self._cur_task)
         self._network.update_fc(self._total_classes)
         logging.info("Learning on {}-{}".format(self._known_classes, self._total_classes))
 
-        train_dataset = data_manager.get_dataset(np.arange(self._known_classes, self._total_classes),source="train", mode="train", m_enable_trsf=True, m_augmentation_trsf=self._trsf)
+        train_dataset = data_manager.get_dataset(np.arange(self._known_classes, self._total_classes), source="train",
+                                                 mode="train")
 
         self.train_dataset = train_dataset
 
-        self.data_manager=data_manager
+        self.data_manager = data_manager
         self.train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=num_workers)
-        test_dataset = data_manager.get_dataset(np.arange(0, self._total_classes), source="test", mode="test" )
+        test_dataset = data_manager.get_dataset(np.arange(0, self._total_classes), source="test", mode="test")
         self.test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=num_workers)
 
-        train_dataset_for_protonet=data_manager.get_dataset(np.arange(self._known_classes, self._total_classes),source="train", mode="train", m_enable_trsf=True, m_augmentation_trsf=self._trsf)
-        self.train_loader_for_protonet = DataLoader(train_dataset_for_protonet, batch_size=self.batch_size, shuffle=True, num_workers=num_workers)
+        train_dataset_for_protonet = data_manager.get_dataset(np.arange(self._known_classes, self._total_classes),
+                                                              source="train", mode="train")
+        self.train_loader_for_protonet = DataLoader(train_dataset_for_protonet, batch_size=self.batch_size,
+                                                    shuffle=True, num_workers=num_workers)
 
         if len(self._multiple_gpus) > 1:
             print('Multiple GPUs')
@@ -105,30 +110,32 @@ class Learner(BaseLearner):
         lora.mark_only_lora_as_trainable(self._network)
         self._network.to(self._device)
         if self._cur_task == 0:
-            if self.args['optimizer']=='sgd':
-                optimizer = optim.SGD(self._network.parameters(), momentum=0.9, lr=self.init_lr,weight_decay=self.weight_decay)
-            elif self.args['optimizer']=='adam':
-                optimizer=optim.AdamW(self._network.parameters(), lr=self.init_lr, weight_decay=self.weight_decay)
-            scheduler=optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.args['tuned_epoch'], eta_min=self.min_lr)
+            if self.args['optimizer'] == 'sgd':
+                optimizer = optim.SGD(self._network.parameters(), momentum=0.9, lr=self.init_lr,
+                                      weight_decay=self.weight_decay)
+            elif self.args['optimizer'] == 'adam':
+                optimizer = optim.AdamW(self._network.parameters(), lr=self.init_lr, weight_decay=self.weight_decay)
+            scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.args['tuned_epoch'],
+                                                             eta_min=self.min_lr)
             self._init_train(train_loader, test_loader, optimizer, scheduler)
             self.construct_dual_branch_network()
             logging.info("Network constructed.")
         else:
             pass
         self.replace_fc(train_loader_for_protonet, self._network, None)
-            
 
     def construct_dual_branch_network(self):
         network = MultiBranchCosineIncrementalNet(self.args, True)
         network.construct_dual_branch_network(self._network)
-        self._network=network.to(self._device)
+        self._network = network.to(self._device)
 
     def _init_lora(self):
         linears = [k for k, m in self._network.named_modules() if type(m).__name__ == 'Linear']
-        for linear in linears:
+        for i in range(0, 5):
+            linear = linears[i]
             in_feat = self._network.get_submodule(linear).in_features
-            out_feat = self._network.get_submodule(linear).in_features
-            l_linear = lora.Linear(in_feat, out_feat)
+            out_feat = self._network.get_submodule(linear).out_features
+            l_linear = lora.Linear(in_feat, out_feat, r=16)
             tokens = linear.split('.')
             sub_tokens = tokens[:-1]
             cur_module = self._network
@@ -182,7 +189,7 @@ class Learner(BaseLearner):
 
         logging.info(info)
 
-    
+
 
 
 
